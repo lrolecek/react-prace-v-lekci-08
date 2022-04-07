9123672d8f5811eeb537d8d750f27732d3810da8
Luděk Roleček <ludek@rolecek.cz>
2022-04-07 19:42:38 +0200

    Aktualizace

--- src/components/Komponenta/index.jsx
+++ src/components/Komponenta/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 const Komponenta = () => {
+
+	const [name, setName] = useState('');
+
+	const handleChange = (event) => {
+		// console.log('zmena v policku');
+		// console.log(event.target.value);
+		setName(event.target.value);
+	}
+
 	return (
 		<div className="komponenta">
 			<h3>Komponenta</h3>
-			<p>Ahoj, já jsem komponenta</p>
+			<input type="text" onChange={handleChange} />
+
+			<p>Jméno je <strong>{name}</strong></p>
 		</div>
 	)
 }
