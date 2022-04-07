b7cc8471e8ccbd8e221f7fef1c580c5ddc40003c
Luděk Roleček <ludek@rolecek.cz>
2022-04-07 20:02:39 +0200

    Aktualizace

--- src/components/Komponenta/index.jsx
+++ src/components/Komponenta/index.jsx
@@ -3,7 +3,8 @@ import './style.css';
 
 const Komponenta = () => {
 
-	const [name, setName] = useState('');
+	const [name, setName] = useState('Erika');
+	const [souhlas, setSouhlas] = useState(false);
 
 	// const handleChange = (event) => {
 	// 	// console.log('zmena v policku');
@@ -14,9 +15,24 @@ const Komponenta = () => {
 	return (
 		<div className="komponenta">
 			<h3>Komponenta</h3>
-			<input type="text" onChange={(event) => {setName(event.target.value)}} />
+			<input type="text" value={name} onChange={(event) => {setName(event.target.value)}} />
 
 			<p>Jméno je <strong>{name}</strong></p>
+
+			{ name.length < 5 && <p>Jmeno musi mit nejmene 5 pismen</p> }
+
+
+			<hr />
+
+			<label>
+				<input
+					type="checkbox"
+					checked={souhlas}
+					onChange={(event) => { setSouhlas(event.target.checked) }}
+				/>
+				Souhlasim s podminkami
+			</label>
+			<button disabled={!souhlas}>Objednat</button>
 		</div>
 	)
 }
